754811b343707290db224f2bc959cb16d034fdfe
tomohiko <illgalsubstance@yahoo.co.jp>
2019-10-02 13:32:21 +0000

    web-gizyutu

---
+++ test.rb
@@ -0,0 +1,23 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+server.mount('/test', WEBrick::HTTPServlet::FileHandler, 'test.html')
+
+# この一行を追記
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+
+server.start
